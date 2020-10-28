6f57e16e56dbc96f9ac1c2f26f0fe5838be62f91
NagibSuaid <nagibmoura@gmail.com>
2020-10-28 14:33:25 -0300

    lab8 sem a main

---
+++ lab8-ode/ode.c
@@ -0,0 +1,59 @@
+#include <math.h>
+#include "ode.h"
+
+static double RungeKuttaStep(double t, double h,double y, double (*f) (double t, double y))
+{
+    double k1=h*f(t,y);
+    double k2=h*f(t+h/2,y+k1/2);
+    double k3=h*f(t+h/2,y+k2/2);
+    double k4=h*f(t+h,y+k3);
+    return y+(k1+2*k2+2*k3+k4)/6.0;
+}
+
+double RungeKutta (double t0, double t1, double h, double y0,
+double (*f) (double t, double y))
+{
+    double t=t0;
+    double y=y0;
+    while( (t1-t) >= h/2 )
+    {
+        y=RungeKuttaStep(t,h,y,f);
+        t=t+h;
+    }
+    return y;
+}
+
+double RungeKuttaAdapt (double t0, double t1, double y0,
+double (*f) (double t, double y), double tol)
+{
+    double t=t0;
+    double y=y0;
+    double h=1.0e-7;
+    while( (t1-t) >= h/2 )
+    {
+       double y1=RungeKuttaStep(t,h,y,f);
+       double y2=RungeKuttaStep(t+h/2,h/2,RungeKuttaStep(t,h/2,y,f),f);
+       double erro=(y2-y1)/15;
+       double fator=pow((tol/fabs(erro)),1/5);
+       if(f>=1)//passo validado
+       {
+           t+=h;
+           if(fator>1.2)
+           {
+               h*=fator;
+           }
+           else
+           {
+               h*=1.2;
+           }
+           
+           y=y2+erro;
+       }
+       else//passo rejeitado
+       {
+           h*=0.8*fator;
+       }
+       
+    }
+    return y;
+}
---
+++ lab8-ode/ode.h
@@ -0,0 +1,10 @@
+#ifndef INCLUDE_ODE
+#define INCLUDE_ODE
+
+double RungeKutta (double t0, double t1, double h, double y0,
+double (*f) (double t, double y));
+
+double RungeKuttaAdapt (double t0, double t1, double y0,
+double (*f) (double t, double y), double tol);
+
+#endif
